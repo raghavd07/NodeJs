e0b2b80913285ac8d20330cbd0d349fa17df389a
Raghav Damera <158565170+raghavd07@users.noreply.github.com>
2026-02-21 16:28:49 +0530

    async, bcrypt

--- index9.js
+++ index9.js
@@ -1,27 +1,42 @@
-import express from "express";
-const app=express();
+import express from "express"
+import bcrypt from "bcrypt"
+const app = express();
 app.listen(8085);
 app.use(express.json())
-let users=[];
-app.get("/",(req,res)=>{
-    res.send("Hello World");
-});
-app.post("/signup",(req,res)=>{
-    const user=req.body;
-    users.push(user);
-    res.send(users);
+
+let users = [];
+
+app.get("/", (req, res) => {
+  res.send("hello world")
 });
 
-app.post("/login",(req,res)=>{
-    const{email,password}=req.body;
-    const found=users.find((user)=>user.email===email&&user.password===password);
-    if(found){
-        res.send("Hello "+found.name);
-    } else{
-        res.send("User not found");
+app.post("/signup", async (req,res)=>{
+    const password = await bcrypt.hash(req.body.password,12)
+    const user= {
+        id: req.body.id,
+        name: req.body.name,
+        email:req.body.email,
+        password:password,
     }
-});
+    users.push(user)
+    res.json(users)
+})
 
-app.get("/",(req,res)=>{
-    res.json(users);
-});
+app.post("/login", async (req,res)=>{
+    const {email, password}= req.body;
+
+    const found = users.find((user)=>
+    user.email===email)
+
+    if (!found){
+        res.status(400).send("Invalid user - not found")
+        return
+    }
+    const isMatch = await bcrypt.compare(password,found.password)
+    if(isMatch){
+        res.send("Hello "+found.name)
+    }
+    else{
+        res.status(400).send("Invalid Password")
+    }
+})
